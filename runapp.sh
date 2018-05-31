aa4592f36a0c68f28d5d9803b912ba67982fc6b7
Chaoshen Hsu <chaoshen_hsu@zoomnow.net>
2018-05-31 14:47:24 +0800

    update run script

--- runapp.sh
+++ runapp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot app'
 cd '/home/ec2-user'
-java -jar target/gs-spring-boot-0.1.0.jar
+java -jar target/gs-spring-boot-0.1.0.jar &
 echo $! > testProcess.pid
 

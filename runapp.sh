6184a7e6ecdb8909975a95434aa37a39d3190fdd
Chaoshen Hsu <chaoshen_hsu@zoomnow.net>
2018-05-31 16:42:19 +0800

    update deploy scripts

--- runapp.sh
+++ runapp.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot app'
 cd '/home/ec2-user'
+if [ -f /home/ec2-user/testProcess.pid ]; then
+    kill -9 `echo /home/ec2-user/testProcess.pid`
+fi
 java -jar target/gs-spring-boot-0.1.0.jar > logs/test.log 2>&1 &
 echo $! > testProcess.pid
 
